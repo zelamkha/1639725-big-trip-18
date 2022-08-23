15ed15e78054a83c58d98d4892c0de696a47f7b6
Zelamkha <zeliwmurt@gmail.com>
2022-08-23 18:22:35 +0300

    Очень тяжело дается. Кажется, я упустил важный блок информации. Не могу схватить

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import SortView from './view/sort-view.js';
+import FilterView from './view/filter-view.js';
+//import CreateFormView from './view/form-create-view.js';
+import CreateFormEditView from './view/form-edit-view.js';
+import ListpointView from './view/new-point-list-view.js';
+import {render} from './render.js';
+import BoardPresenter from './presenter/board-presenter.js';
+const siteFilterElement = document.querySelector('.trip-controls__filters');
+const siteSortElement = document.querySelector('.trip-events');
+const boardPresenter = new BoardPresenter();
+//const siteHeaderElement = document.querySelector('.btn--yellow');
+render(new SortView(), siteSortElement);
+render(new FilterView(), siteFilterElement);
+//render(new CreateFormView(), siteHeaderElement);
+render(new CreateFormEditView(), siteSortElement);
+render(new ListpointView, siteSortElement);
+
+boardPresenter.init(siteSortElement);
---
+++ src/presenter/board-presenter.js
@@ -0,0 +1,22 @@
+import BoardView from '../view/content-view.js';
+//import SortView from '../view/sort-view.js';
+import ListpointView from '../view/new-point-list-view.js';
+import CreateFormEditView from '../view/form-edit-view.js';
+import {render} from '../render.js';
+
+export default class BoardPresenter {
+  boardComponent = new BoardView();
+  taskListComponent = new ListpointView();
+
+  init = (boardContainer) => {
+    this.boardContainer = boardContainer;
+
+    render(this.boardComponent, this.boardContainer);
+    render(new CreateFormEditView(), this.boardComponent.getElement());
+    render(this.taskListComponent, this.boardComponent.getElement());
+
+    for (let i = 0; i < 3; i++) {
+      render(new ListpointView(), this.taskListComponent.getElement());
+    }
+  };
+}
